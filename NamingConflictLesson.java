3f61eac1e0245749aedbdbc0a48f4408a0225982
ecmanliguis-118a <ecmanliguis@gmail.com>
2026-05-23 09:22:34 +0000

    edited

--- NamingConflictLesson.java
+++ NamingConflictLesson.java
@@ -1,33 +1,52 @@
 package chapter1;
 
-public class PrimitivesLesson {
+public class ConstructorLesson1 {
     public static void main(String[] args) {
-        boolean isRaining =false; // true : false
-        char c = 65535; ////2^16 'a' 1 //no negative value
-        byte b = 127; // 2^8=256 signed -128,0 127
-        short s = 32767; //2^16=65536 signed -32768 ,0 32767
-        int i = 1000000; //2^32=4b signed
-        long l = 1; //2^64
-        float f = 1.0f; //2^32 floating point value
-        double d = 1.0; //2^64 floating point value
-        System.out.println(c);
-        
-        System.out.println("Character.MAX_VALUE="+ Character.MAX_VALUE);
-        System.out.println("Character.MIN_VALUE="+ Character.MIN_VALUE);
-        System.out.println("Byte.MAX_VALUE="+ Byte.MAX_VALUE);
-        System.out.println("Byte.MIN_VALUE="+ Byte.MIN_VALUE);
-        System.out.println("Short.MAX_VALUE="+ Short.MAX_VALUE);
-        System.out.println("Short.MIN_VALUE="+ Short.MIN_VALUE);
-        System.out.println("Integer.MAX_VALUE="+ Integer.MAX_VALUE);
-        System.out.println("Integer.MIN_VALUE="+ Integer.MIN_VALUE);
-        System.out.println("Long.MAX_VALUE="+ Long.MAX_VALUE);
-        System.out.println("Long.MIN_VALUE="+ Long.MIN_VALUE);
-        System.out.println("Float.MAX_VALUE="+ Float.MAX_VALUE);
-        System.out.println("Float.MIN_VALUE="+ Float.MIN_VALUE);
-        System.out.println("Double.MAX_VALUE="+ Double.MAX_VALUE);
-        System.out.println("Double.MIN_VALUE="+ Double.MIN_VALUE);
-        System.out.println(1_0_00_00_0.0_0);
+        Chick andoks = new Chick();
+        Chick chooks = new Chick();
+        Chick gannam = andoks;
+        andoks.Chick();
+        Chicken jolChicken = new Chicken();
+        System.out.println("end of program");
+    }
 
+}
 
+class Chick{ //🥚
+    public Chick(){
+        System.out.println("🐣 in constructor");
+    }
+    public void Chick(){
+        System.out.println("🐥💬 I'm a method");
+    }
+}
+
+class Chicken{
+    int numEggs = 0; //initialized on line, class variable or instance variable,
+    String name;
+    public Chicken(){
+        name = "Duke"; //initialized on constructor
+    }
+}
+
+class Swan{
+    int numEggs;
+    public static void main(String[] args) {
+        Swan mother = new Swan();
+        mother.numEggs = 4;//writting object fields
+        System.out.println(mother.numEggs); //reading object fields
+    }
+
+}
+class Name{
+    String first = "Theodore";
+    String last = "Moose";
+    String full = first+last;
+    public static void main(String[] args) {
+        Name name1 = new Name();
+        System.out.println(name1.full);
+        name1.first = "Marc";
+        name1.last = "Yim";
+        System.out.println(name1.full);
     }
 }
